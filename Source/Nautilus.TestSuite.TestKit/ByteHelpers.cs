ec4b69ab1e8a6fd9a74ce9e23154be9eb446e0cd
cjdsellers <cjdsellers@protonmail.com>
2018-07-17 06:50:00 +1000

    Add MsgPack tests.

---
+++ Source/Nautilus.TestSuite.TestKit/ByteHelpers.cs
@@ -0,0 +1,32 @@
+//--------------------------------------------------------------------------------------------------
+// <copyright file="ByteHelpers.cs" company="Nautech Systems Pty Ltd">
+//  Copyright (C) 2015-2018 Nautech Systems Pty Ltd. All rights reserved.
+//  The use of this source code is governed by the license as found in the LICENSE.txt file.
+//  http://www.nautechsystems.net
+// </copyright>
+//--------------------------------------------------------------------------------------------------
+
+namespace Nautilus.TestSuite.TestKit
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public static class ByteHelpers
+    {
+        public static byte[] StringToByteArray(string hex) {
+            return Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            var hex = new StringBuilder(ba.Length * 2);
+            foreach (var b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
---
+++ Source/Nautilus.TestSuite.UnitTests/InfrastructureTests/MsgPackTests/MsgPackSerializerTests.cs
@@ -0,0 +1,63 @@
+// -------------------------------------------------------------------------------------------------
+// <copyright file="MsgPackSerializerTests.cs" company="Nautech Systems Pty Ltd.">
+//   Copyright (C) 2015-2018 Nautech Systems Pty Ltd. All rights reserved.
+//   The use of this source code is governed by the license as found in the LICENSE.txt file.
+//   http://www.nautechsystems.net
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace Nautilus.TestSuite.UnitTests.InfrastructureTests.MsgPackTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using global::MsgPack;
+    using Nautilus.MsgPack;
+    using Nautilus.TestSuite.TestKit;
+    using Xunit;
+
+    [SuppressMessage("StyleCop.CSharp.NamingRules", "*", Justification = "Reviewed. Suppression is OK within the Test Suite.")]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "Reviewed. Suppression is OK within the Test Suite.")]
+    public class MsgPackSerializerTests
+    {
+        [Fact]
+        internal void Test_can_serialize_and_deserialize()
+        {
+            // Arrange
+            var msgPack = new MessagePackObjectDictionary
+            {
+                {new MessagePackObject("header"), "the_msg_header"},
+                {new MessagePackObject("value1"), 42}
+            }.Freeze();
+
+            // Act
+            var packed = MsgPackSerializer.Serialize(msgPack);
+            var unpacked = MsgPackSerializer.Deserialize<MessagePackObjectDictionary>(packed);
+
+            // Assert
+            Assert.Equal(msgPack.Count, unpacked.Count);
+        }
+
+        [Fact]
+        internal void Test_can_deserialize_from_python_packed_msg()
+        {
+            // Arrange
+            // From Python dict.
+            // message = {
+            // 'header': 'order_cancelled',
+            // 'symbol': 'AUDUSD.FXCM',
+            // 'order_id': 'O123456',
+            // 'timestamp': '1970-01-01T00:00:00.000Z'}
+            var python_hex = "84a6686561646572af6f726465725f63616e63656c6c6564a673796d626f6cab4155445553442e4658434da86f726465725f6964a74f313233343536a974696d657374616d70b8313937302d30312d30315430303a30303a30302e3030305a";
+            var data = ByteHelpers.StringToByteArray(python_hex);
+
+            // Act
+            var unpacked = MsgPackSerializer.Deserialize<MessagePackObjectDictionary>(data);
+
+            // Assert
+            Assert.Equal(4, unpacked.Count);
+            Assert.Equal("order_cancelled", unpacked["header"]);
+            Assert.Equal("AUDUSD.FXCM", unpacked["symbol"]);
+            Assert.Equal("O123456", unpacked["order_id"]);
+            Assert.Equal("1970-01-01T00:00:00.000Z", unpacked["timestamp"]);
+        }
+    }
+}
