e5745b1622e28c4c4d11fb9ae4e017019205f749
cjdsellers <cjdsellers@protonmail.com>
2018-08-07 07:56:07 +1000

    Remove custom QuickFix projects.

--- Source/QuickFix/Config/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuickFix.Config
-{
-    [System.Obsolete("This class is not used in Quickfix/n.  Ignore it.")]
-    public class Config
-    {
-        public Config(string configFileName)
-        { }
-    }
-}
