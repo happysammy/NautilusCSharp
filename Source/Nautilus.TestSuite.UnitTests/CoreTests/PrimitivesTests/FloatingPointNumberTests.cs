c162ee59220f7d8c9c8dd2f1368ed2fc8262b05d
cjdsellers <cjdsellers@protonmail.com>
2019-05-09 20:47:42 +1000

    Add unit test files.

---
+++ Source/Nautilus.TestSuite.UnitTests/CoreTests/PrimitivesTests/FloatingPointNumberTests.cs
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------------------------------------------
+// <copyright file="FloatingPointNumberTests.cs" company="Nautech Systems Pty Ltd">
+//  Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
+//  The use of this source code is governed by the license as found in the LICENSE.txt file.
+//  http://www.nautechsystems.net
+// </copyright>
+//--------------------------------------------------------------------------------------------------
+
+namespace Nautilus.TestSuite.UnitTests.CoreTests.PrimitivesTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Xunit;
+
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK within the Test Suite.")]
+    public class FloatingPointNumberTests
+    {
+        [Fact]
+        internal void Unit_Test_One()
+        {
+            // Arrange
+
+            // Act
+
+
+            // Assert
+        }
+    }
+}
