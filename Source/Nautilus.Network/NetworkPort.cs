c747924533b88974bdf7c3402db188de3455b010
cjdsellers <cjdsellers@protonmail.com>
2019-05-11 08:42:51 +1000

    Add comment.

--- Source/Nautilus.Network/NetworkPort.cs
+++ Source/Nautilus.Network/NetworkPort.cs
@@ -1,5 +1,5 @@
 // -------------------------------------------------------------------------------------------------
-// <copyright file="Port.cs" company="Nautech Systems Pty Ltd">
+// <copyright file="NetworkPort.cs" company="Nautech Systems Pty Ltd">
 //   Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
 //   The use of this source code is governed by the license as found in the LICENSE.txt file.
 //   http://www.nautechsystems.net
