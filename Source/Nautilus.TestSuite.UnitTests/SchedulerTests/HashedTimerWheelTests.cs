b61f1e6ee6e087c7b6fb92df1c2ef72d9cc4ae07
cjdsellers <cjdsellers@protonmail.com>
2019-05-17 12:06:12 +1000

    Add tests.

--- Source/Nautilus.TestSuite.UnitTests/SchedulerTests/HashedTimerWheelTests.cs
+++ Source/Nautilus.TestSuite.UnitTests/SchedulerTests/HashedTimerWheelTests.cs
@@ -10,8 +10,10 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using System.Threading.Tasks;
     using Nautilus.Scheduler;
+    using Nautilus.TestSuite.TestKit;
     using Nautilus.TestSuite.TestKit.TestDoubles;
     using Xunit;
     using Xunit.Abstractions;
@@ -20,6 +22,8 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
     public class HashedTimerWheelTests
     {
         private readonly ITestOutputHelper output;
+        private readonly MockLoggingAdapter loggingAdapter;
+        private readonly MockMessagingAgent testReceiver;
         private readonly HashedWheelTimerScheduler scheduler;
 
         private int testActionCount = 0;
@@ -31,6 +35,9 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
 
             var containerFactory = new StubComponentryContainerFactory();
             var container = containerFactory.Create();
+            this.loggingAdapter = containerFactory.LoggingAdapter;
+            this.testReceiver = new MockMessagingAgent();
+            this.testReceiver.RegisterHandler<string>(this.testReceiver.OnMessage);
             this.scheduler = new HashedWheelTimerScheduler(container);
         }
 
@@ -53,7 +60,7 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
             // Act
             this.scheduler.ScheduleOnce(TimeSpan.Zero, this.Run);
 
-            Task.Delay(100).Wait(); // Wait for potential action to fire.
+            Task.Delay(100).Wait(); // Wait for potential action(s) to fire.
 
             // Assert
             Assert.Equal(1, this.testActionCount);
@@ -66,7 +73,7 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
             // Act
             this.scheduler.ScheduleOnceCancelable(TimeSpan.Zero, this.Run);
 
-            Task.Delay(100).Wait(); // Wait for potential action to fire.
+            Task.Delay(100).Wait(); // Wait for potential action(s) to fire.
 
             // Assert
             Assert.Equal(1, this.testActionCount);
@@ -80,7 +87,7 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
             var cancelable = this.scheduler.ScheduleOnceCancelable(TimeSpan.Zero, this.Run);
             cancelable.Cancel();
 
-            Task.Delay(100).Wait(); // Wait for potential action to fire.
+            Task.Delay(100).Wait(); // Wait for potential action(s) to fire.
 
             // Assert
             Assert.Equal(0, this.testActionCount);
@@ -94,10 +101,10 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
             this.scheduler.ScheduleRepeatedly(TimeSpan.Zero, TimeSpan.FromMilliseconds(10), this.Run);
 
             // Takes approx 50ms to spool up the scheduler.
-            Task.Delay(100).Wait(); // Wait for potential action to fire.
+            Task.Delay(100).Wait(); // Wait for potential action(s) to fire.
 
             // Assert
-            Assert.Equal(5, this.testActionCount);
+            Assert.True(this.testActionCount > 4);
         }
 
         [Fact]
@@ -108,10 +115,10 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
             this.scheduler.ScheduleRepeatedlyCancelable(TimeSpan.Zero, TimeSpan.FromMilliseconds(10), this.Run);
 
             // Takes approx 50ms to spool up the scheduler.
-            Task.Delay(100).Wait(); // Wait for potential action to fire.
+            Task.Delay(100).Wait(); // Wait for potential action(s) to fire.
 
             // Assert
-            Assert.Equal(5, this.testActionCount);
+            Assert.True(this.testActionCount > 4);
         }
 
         [Fact]
@@ -123,10 +130,92 @@ namespace Nautilus.TestSuite.UnitTests.SchedulerTests
             cancelable.Cancel();
 
             // Takes approx 50ms to spool up the scheduler.
-            Task.Delay(100).Wait(); // Wait for potential action to fire.
+            Task.Delay(100).Wait(); // Wait for potential action(s) to fire.
 
             // Assert
             Assert.Equal(0, this.testActionCount);
+        }
+
+        [Fact]
+        internal void ScheduleSendOnce_ThenMessageShouldBeSent()
+        {
+            // Arrange
+            // Act
+            this.scheduler.ScheduleSendOnce(TimeSpan.Zero, this.testReceiver.Endpoint, "TEST", this.scheduler.Endpoint);
+
+            Task.Delay(100).Wait(); // Wait for potential message(s) to send.
+
+            // Assert
+            Assert.Single(this.testReceiver.Messages);
+            Assert.Equal("TEST", this.testReceiver.Messages[0]);
+        }
+
+        [Fact]
+        internal void ScheduleSendOnceCancelable_WhenNotCancelled_ThenMessageShouldBeSent()
+        {
+            // Arrange
+            // Act
+            this.scheduler.ScheduleSendOnceCancelable(TimeSpan.Zero, this.testReceiver.Endpoint, "TEST", this.scheduler.Endpoint);
+
+            Task.Delay(100).Wait(); // Wait for potential message(s) to send.
+
+            // Assert
+            Assert.Single(this.testReceiver.Messages);
+            Assert.Equal("TEST", this.testReceiver.Messages[0]);
+        }
+
+        [Fact]
+        internal void ScheduleSendOnceCancelable_WhenCancelled_ThenMessageShouldNotBeSent()
+        {
+            // Arrange
+            // Act
+            var cancelable = this.scheduler.ScheduleSendOnceCancelable(TimeSpan.Zero, this.testReceiver.Endpoint, "TEST", this.scheduler.Endpoint);
+            cancelable.Cancel();
+            Task.Delay(100).Wait(); // Wait for potential message(s) to send.
+
+            // Assert
+            Assert.Empty(this.testReceiver.Messages);
+        }
+
+        [Fact]
+        internal void ScheduleSendRepeatedly_ThenMessageShouldBeSentRepeatedly()
+        {
+            // Arrange
+            // Act
+            this.scheduler.ScheduleSendRepeatedly(TimeSpan.Zero, TimeSpan.FromMilliseconds(10), this.testReceiver.Endpoint, "TEST", this.scheduler.Endpoint);
+
+            Task.Delay(100).Wait(); // Wait for potential message(s) to send.
+
+            // Assert
+            Assert.True(this.testReceiver.Messages.Count > 4);
+            Assert.Equal("TEST", this.testReceiver.Messages[4]);
+        }
+
+        [Fact]
+        internal void ScheduleSendRepeatedlyCancelable_WhenNotCancelled_ThenMessageShouldBeSentRepeatedly()
+        {
+            // Arrange
+            // Act
+            var cancelable = this.scheduler.ScheduleSendRepeatedlyCancelable(TimeSpan.Zero, TimeSpan.FromMilliseconds(10), this.testReceiver.Endpoint, "TEST", this.scheduler.Endpoint);
+
+            Task.Delay(100).Wait(); // Wait for potential message(s) to send.
+
+            // Assert
+            Assert.True(this.testReceiver.Messages.Count > 4);
+            Assert.Equal("TEST", this.testReceiver.Messages[4]);
+        }
+
+        [Fact]
+        internal void ScheduleSendRepeatedlyCancelable_WhenCancelled_ThenMessageShouldNotBeSentRepeatedly()
+        {
+            // Arrange
+            // Act
+            var cancelable = this.scheduler.ScheduleSendRepeatedlyCancelable(TimeSpan.Zero, TimeSpan.FromMilliseconds(10), this.testReceiver.Endpoint, "TEST", this.scheduler.Endpoint);
+            cancelable.Cancel();
+            Task.Delay(100).Wait(); // Wait for potential message(s) to send.
+
+            // Assert
+            Assert.Empty(this.testReceiver.Messages);
         }
 
         private void Run()
