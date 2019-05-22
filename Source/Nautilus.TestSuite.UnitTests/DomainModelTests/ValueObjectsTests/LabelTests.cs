1a297f2750a739a4c9142730e155f5ad40879213
cjdsellers <cjdsellers@protonmail.com>
2019-05-22 11:58:16 +1000

    Fix header.

--- Source/Nautilus.TestSuite.UnitTests/DomainModelTests/ValueObjectsTests/LabelTests.cs
+++ Source/Nautilus.TestSuite.UnitTests/DomainModelTests/ValueObjectsTests/LabelTests.cs
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// <copyright file="ValidStringTests.cs" company="Nautech Systems Pty Ltd">
+// <copyright file="LabelTests.cs" company="Nautech Systems Pty Ltd">
 //  Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
 //  The use of this source code is governed by the license as found in the LICENSE.txt file.
 //  http://www.nautechsystems.net
