816c389862ecc2228df01fb9d65b4b6759febb3f
cjdsellers <cjdsellers@protonmail.com>
2019-06-02 18:23:55 +1000

    Add response.

--- Source/Nautilus.Data/Messages/Responses/BarDataResponse.cs
+++ Source/Nautilus.Data/Messages/Responses/BarDataResponse.cs
@@ -1,50 +1,54 @@
 //--------------------------------------------------------------------------------------------------
-// <copyright file="Response.cs" company="Nautech Systems Pty Ltd">
+// <copyright file="BarDataResponse.cs" company="Nautech Systems Pty Ltd">
 //  Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
 //  The use of this source code is governed by the license as found in the LICENSE.txt file.
 //  http://www.nautechsystems.net
 // </copyright>
 //--------------------------------------------------------------------------------------------------
 
-namespace Nautilus.Core
+namespace Nautilus.Data.Messages.Responses
 {
     using System;
+    using Nautilus.Core;
     using Nautilus.Core.Annotations;
     using Nautilus.Core.Correctness;
+    using Nautilus.Data.Messages.Requests;
+    using Nautilus.Data.Types;
     using NodaTime;
 
     /// <summary>
-    /// The base class for all <see cref="Response"/> messages.
+    /// Represents a request for historical bar data.
     /// </summary>
     [Immutable]
-    public abstract class Response : Message
+    public sealed class BarDataResponse : Response
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="Response"/> class.
+        /// Initializes a new instance of the <see cref="BarDataResponse"/> class.
         /// </summary>
-        /// <param name="type">The response type.</param>
+        /// <param name="barData">The response bar data.</param>
         /// <param name="correlationId">The response correlation identifier.</param>
         /// <param name="id">The response identifier.</param>
         /// <param name="timestamp">The response timestamp.</param>
-        protected Response(
-            Type type,
+        public BarDataResponse(
+            BarDataFrame barData,
             Guid correlationId,
             Guid id,
             ZonedDateTime timestamp)
             : base(
-                type,
+                typeof(BarDataRequest),
+                correlationId,
                 id,
                 timestamp)
         {
             Debug.NotDefault(id, nameof(id));
             Debug.NotDefault(timestamp, nameof(timestamp));
 
-            this.CorrelationId = correlationId;
+            this.BarData = barData;
         }
 
         /// <summary>
-        /// Gets the responses correlation identifier.
+        /// Gets the responses <see cref="BarDataFrame"/>.
         /// </summary>
-        public Guid CorrelationId { get; }
+        public BarDataFrame BarData { get; }
     }
 }
