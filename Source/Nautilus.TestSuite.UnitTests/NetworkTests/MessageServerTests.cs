703d424de5a67c733c96fa81687518daeabf5f12
Christopher Sellers <cjdsellers@protonmail.com>
2019-06-05 16:04:30 +1000

    Change test names.

--- Source/Nautilus.TestSuite.UnitTests/NetworkTests/MessageServerTests.cs
+++ Source/Nautilus.TestSuite.UnitTests/NetworkTests/MessageServerTests.cs
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// <copyright file="ServerTests.cs" company="Nautech Systems Pty Ltd">
+// <copyright file="MessageServerTests.cs" company="Nautech Systems Pty Ltd">
 //  Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
 //  The use of this source code is governed by the license as found in the LICENSE.txt file.
 //  http://www.nautechsystems.net
@@ -24,7 +24,7 @@ namespace Nautilus.TestSuite.UnitTests.NetworkTests
     using Xunit.Abstractions;
 
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK within the Test Suite.")]
-    public class ServerTests
+    public class MessageServerTests
     {
         private readonly ITestOutputHelper output;
         private readonly IComponentryContainer container;
@@ -32,7 +32,7 @@ namespace Nautilus.TestSuite.UnitTests.NetworkTests
         private readonly MockSerializer serializer;
         private readonly MsgPackResponseSerializer responseSerializer;
 
-        public ServerTests(ITestOutputHelper output)
+        public MessageServerTests(ITestOutputHelper output)
         {
             // Fixture Setup
             this.output = output;
