20a8ff64437dca6a0e2cbf025e93df2fea92ee3a
cjdsellers <cjdsellers@protonmail.com>
2019-07-28 08:34:08 +1000

    Change company url.

--- Source/Nautilus.Core/Annotations/InvalidValueAttribute.cs
+++ Source/Nautilus.Core/Annotations/InvalidValueAttribute.cs
@@ -2,7 +2,7 @@
 // <copyright file="InvalidValueAttribute.cs" company="Nautech Systems Pty Ltd">
 //  Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
 //  The use of this source code is governed by the license as found in the LICENSE.txt file.
-//  http://www.nautechsystems.io
+//  https://nautechsystems.io
 // </copyright>
 //--------------------------------------------------------------------------------------------------
 
